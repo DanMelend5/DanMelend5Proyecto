e3cea3e88308b3aa7abaa74e8084bb17db44cbc1
DanMelend5 <danmelendez1986@gmail.com>
2023-11-28 23:11:37 -0300

    it works!

--- js/myScript.js
+++ js/myScript.js
@@ -5,7 +5,7 @@ const valorTicket = 200;
 
 // Se definen los valores por categoría
 let descuentoEstudiante = 80;
-let descuentoTrainee= 50;
+let descuentoTrainee = 50;
 let descuentoJunior = 15;
 
 // Elementos en variables
@@ -19,29 +19,23 @@ let btnBorrar = document.getElementById("btnBorrar");
 
 
 // Elementos de mensajes de Error
-let divNombreCampoVacio     = document.getElementById("divNombreCampoVacio");
-let divApellidoCampoVacio   = document.getElementById("divApellidoCampoVacio");
-let divCorreoCampoVacio     = document.getElementById("divCorreoCampoVacio");
-let divCorreoInvalido       = document.getElementById("divCorreoInvalido");
-let divErrorSeleccion       = document.getElementById("divErrorSeleccion");
-let divCantidadCampoVacio   = document.getElementById("divCantidadCampoVacio");
+let divNombreCampoVacio = document.getElementById("divNombreCampoVacio");
+let divApellidoCampoVacio = document.getElementById("divApellidoCampoVacio");
+let divCorreoInvalido = document.getElementById("divCorreoInvalido");
+let divErrorSeleccion = document.getElementById("divErrorSeleccion");
+let divCantidadCampoVacio = document.getElementById("divCantidadCampoVacio");
 let divErrorNumerodeTickets = document.getElementById("divErrorNumerodeTickets");
 
-
+GIT
 
 // Funcion para quitar error del formulario.
 function quitarMensajeError() {
-    let arrayNodosFormulario = document.querySelectorAll(".form-control, .form-select");
-    for (let i = 0; i < arrayNodosFormulario.length; i++) {
-        arrayNodosFormulario[i].classList.remove(".invalid-feedback");
-        console.log("Removed 'is-invalid' class");
-    }
-
-    let arrayDivNodosErrores = document.querySelectorAll(".invalid-feedback");
-    for (let i = 0; i < arrayDivNodosErrores.length; i++) {
-        arrayDivNodosErrores[i].classList.remove(".mensajeError");
-        console.log("Removed 'mensajeError' class");
-    }
+	let arrayNodosFormulario = document.querySelectorAll(".form-control, .form-select");
+	for (let i = 0; i < arrayNodosFormulario.length; i++) {
+		arrayNodosFormulario[i].classList.remove("is-invalid");
+		arrayNodosFormulario[i].classList.remove("invalid-feedback");
+		console.log("Removed 'is-invalid' class");
+	}
 }
 
 
@@ -50,94 +44,94 @@ function quitarMensajeError() {
 quitarMensajeError();
 
 // Calcular precio
-function totalAPagar(){
+function totalAPagar() {
 
-    if (nombre.value === ""){
-        nombre.classList.add("is-invalid");
-        divNombreCampoVacio.classList.add("mensajeError");
-        nombre.focus();
-        return;
-    }
+	if (nombre.value === "") {
+		nombre.classList.add("is-invalid");
+		divNombreCampoVacio.classList.add("invalid-feedback");
+		nombre.focus();
+		return;
+	}
 
-    if (apellido.value === ""){
-        apellido.classList.add("is-invalid");
-        divApellidoCampoVacio.classList.add("mensajeError");
-        apellido.focus();
-        return;
-    }
+	if (apellido.value === "") {
+		apellido.classList.add("is-invalid");
+		divApellidoCampoVacio.classList.add("invalid-feedback");
+		apellido.focus();
+		return;
+	}
 
-    if (correo.value === ""){
-        correo.classList.add("is-invalid");
-        divCorreoCampoVacio.classList.add("mensajeError");
-        correo.focus();
-        return;
-    }
+	if (correo.value === "") {
+		correo.classList.add("is-invalid");
+		divCorreoInvalido.classList.add("invalid-feedback");
+		correo.focus();
+		return;
+	}
 
-        //para determinar que el correo es válido
-    const correoValido = email => {
-        const correoRegex = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
-        return correoRegex.test(email);
-        }
-    
-    if (!correoValido(correo.value)) {
-        correo.classList.add("is-invalid");
-        divCorreoInvalido.classList.add("mensajeError"); 
-        correo.focus();
-        return;
-    }
+	//para determinar que el correo es válido
+	const correoValido = email => {
+		const correoRegex = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+		return correoRegex.test(email);
+	}
 
-    //Numero de tickets
-    if ((cantidadTicket.value =="")){
-        divCantidadCampoVacio.classList.add("mensajeError");  
-        cantidadTicket.focus();
-        return; 
-    }
+	if (!correoValido(correo.value)) {
+		correo.classList.add("is-invalid");
+		divCorreoInvalido.classList.add("invalid-feedback");
+		correo.focus();
+		return;
+	}
 
-    if (isNaN (cantidadTicket.value) || (cantidadTicket.value <= 0)) {
-        divErrorNumerodeTickets.classList.add("mensajeError");
-        cantidadTicket.focus();
-        return; 
+	//Numero de tickets
+	if ((cantidadTicket.value == "")) {
+		divCantidadCampoVacio.classList.add("invalid-feedback");
+		cantidadTicket.focus();
+		return;
+	}
 
-    }
-	
-	  //Multiplicar cantidad de tickets
-	  let totalPrecioTickets = (cantidadTicket.value * valorTicket);
-		
+	if (isNaN(cantidadTicket.value) || (cantidadTicket.value <= 0)) {
+		divErrorNumerodeTickets.classList.add("invalid-feedback");
+		cantidadTicket.focus();
+		return;
 
-    if (categoriaSeleccion.value == "----"){
-        categoriaSeleccion.classList.add("is-invalid");
-        divErrorSeleccion.classList.add("mensajeError");
-        categoriaSeleccion.focus();
-        return;
-    }
-    
-    
-  
+	}
 
-    switch (categoriaSeleccion.value){
+	//Multiplicar cantidad de tickets
+	let totalPrecioTickets = (cantidadTicket.value * valorTicket);
+
+
+	if (categoriaSeleccion.value == "----") {
+		categoriaSeleccion.classList.add("is-invalid");
+		divErrorSeleccion.classList.add("invalid-feedback");
+		categoriaSeleccion.focus();
+		return;
+	}
+
+
+
+
+	switch (categoriaSeleccion.value) {
 		case "Otro":
 			totalPrecioTickets;
 			console.log("otro " + totalPrecioTickets);
-			
+
 			break;
 
-        case "Estudiante":
-            totalPrecioTickets = totalPrecioTickets - (descuentoEstudiante / 100 * totalPrecioTickets);
-			console.log ("Estudiante");
-            break;
-        case "Trainee":
-            totalPrecioTickets = totalPrecioTickets - (descuentoTrainee / 100 * totalPrecioTickets);
-			console.log ("trainee " +  totalPrecioTickets);
-            break;
-        case "Junior":
-            totalPrecioTickets = totalPrecioTickets - (descuentoJunior / 100 * totalPrecioTickets);
-			console.log ("junior");
-            break;
-    }
+		case "Estudiante":
+			totalPrecioTickets = totalPrecioTickets - (descuentoEstudiante / 100 * totalPrecioTickets);
+			console.log("Estudiante");
+			break;
+		case "Trainee":
+			totalPrecioTickets = totalPrecioTickets - (descuentoTrainee / 100 * totalPrecioTickets);
+			console.log("trainee " + totalPrecioTickets);
+			break;
+		case "Junior":
+			totalPrecioTickets = totalPrecioTickets - (descuentoJunior / 100 * totalPrecioTickets);
+			console.log("junior");
+			break;
+	}
 
-    // Resultado aparece en el campo "total$"
-   // Resultado aparece en el campo "total$"
- 	totalPago.value = totalPrecioTickets;
+	// Resultado aparece en el campo "total$"
+	// Resultado aparece en el campo "total$"
+	totalPago.value = totalPrecioTickets;
 
 
 }
@@ -150,8 +144,11 @@ btnComprar.addEventListener('click', totalAPagar);
 // Boton Borrar
 
 function reset_totalAPagar() {
-   	quitarMensajeError();
-    totalPago.value = "";
+	quitarMensajeError();
+
+	let formulario = document.querySelector('form');
+	formulario.reset();
+	totalPago.value = "";
 }
- 
+
 btnBorrar.addEventListener('click', reset_totalAPagar);
